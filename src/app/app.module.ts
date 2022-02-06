026fc5d434180627dd70415a8e821ee39c9ad662
aehrlich1 <anatol.ehrlich@gmail.com>
2022-02-06 12:38:24 +0100

    added top-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
